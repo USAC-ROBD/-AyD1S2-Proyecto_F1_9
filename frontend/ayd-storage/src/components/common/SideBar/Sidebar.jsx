a39039daf6cd7abb9211c880746ca8a4c40dfc1a
Steven González <stevengonzalez088@gmail.com>
2024-09-11 18:22:16 -0600

    [UPDATE] Se cambio responsividad de componente Sidebar

--- frontend/ayd-storage/src/components/common/SideBar/Sidebar.jsx
+++ frontend/ayd-storage/src/components/common/SideBar/Sidebar.jsx
@@ -1,81 +1,74 @@
 import React, { useState, useEffect } from "react";
 import '../../../styles/Sidebar.css';
-import { Avatar, Drawer, List, Stack, Toolbar, Typography } from "@mui/material";
-import { Link } from "react-router-dom";
-import sizeConfigs from "../../../configs/sizeConfigs";
+import { List, Stack, Toolbar, Typography, Divider } from "@mui/material";
 import assets from "../../../assets";
-import colorConfigs from "../../../configs/colorConfigs";
-// import appRoutes from "../../routes/appRoutes";
-// import structDB from "./StructDB";
 import SidebarItem from "./SidebarItem";
 import SidebarItemCollapse from "./SidebarItemCollapse";
-import StorageOutlinedIcon from '@mui/icons-material/StorageOutlined';
 import HomeIcon from '@mui/icons-material/Home';
 
 const Sidebar = () => {
   const [structDB, setStructDB] = useState([]);
-  const [rotateIcon, setRotateIcon] = useState(false);
 
-    const sideBarItems = [
-        {
-            level: 0,
-            state: "Home",  // Estado para activar el color del item seleccionado, debe ser igual al estado del componente en el archivo de rutas
-            sidebarProps: {
-                icon: <HomeIcon />,
-                displayText: "Home",
-            }
-
-        },
-    ]
+  const sideBarItems = [
+    {
+      level: 0,
+      state: "Home",  // Estado para activar el color del item seleccionado, debe ser igual al estado del componente en el archivo de rutas
+      sidebarProps: {
+        icon: <HomeIcon />,
+        displayText: "Home",
+      },
+      userType: 1, // Tipo de usuario que puede ver este item. 1: Administrador, 2: Empleado, 3: Cliente         
+    },
+  ]
 
   useEffect(() => {
-     setStructDB(sideBarItems);
+    //TODO: Verificar en el localStorage el tipo de usuario y mostrar los items correspondientes con un filter
+    setStructDB(sideBarItems);
   }, []);
 
   return (
-    <Drawer
-      variant="permanent"
-      sx={{
-        width: sizeConfigs.sidebar.width,
-        flexShrink: 0,
-        "& .MuiDrawer-paper": {
-          width: sizeConfigs.sidebar.width,
-          boxSizing: "border-box",
-          borderRight: "0px",
-          backgroundColor: colorConfigs.sidebar.bg,
-          color: colorConfigs.sidebar.color,
-        },
-      }}
-    >
-      <List disablePadding>
-        <Toolbar sx={{
-          marginBottom: "20px"
-        }}>
-          <Stack
-            sx={{ width: "100%", marginTop: "20px" }}
-            direction="column"
-            justifyContent="center"
-            alignItems="center"
-          >
-            <Link to="/">
-              <Avatar src={assets.images.logo} />
-            </Link>
-            <Typography variant="h6" sx={{ marginTop: "20px" }}>
-              AYD Storage
-            </Typography>
-          </Stack>
-        </Toolbar>
-        {structDB.map((item, index) => (
-          item.sidebarProps ? (
-            item.child ? (
-              <SidebarItemCollapse item={item} key={index} />
-            ) : (
-              <SidebarItem item={item} key={index} />
-            )
-          ) : null
-        ))}
-      </List>
-    </Drawer>
+    <List disablePadding>
+      <Toolbar sx={{
+        marginBottom: "20px"
+      }}>
+        <Stack
+          sx={{ width: "100%", marginTop: "20px" }}
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Typography variant="h6" sx={{ marginTop: "20px" }}>
+            AYD
+          </Typography>
+          <Typography variant="h6">
+            Storage
+          </Typography>
+        </Stack>
+      </Toolbar>
+      <Divider variant="middle" component="li"
+        sx={{
+          marginTop: "5px",
+          marginBottom: "5px",
+          borderColor: "rgb(255 255 255 / 30%)"
+        }}
+      />
+      {structDB.map((item, index) => (
+        item.sidebarProps ? (
+          item.child ? (
+            <SidebarItemCollapse item={item} key={index} />
+          ) : (
+            <SidebarItem item={item} key={index} />
+          )
+        ) : null
+      ))}
+      <Divider variant="middle" component="li"
+        sx={{
+          marginTop: "5px",
+          marginBottom: "5px",
+          borderColor: "rgb(255 255 255 / 30%)"
+        }}
+      />
+    </List>
   );
 };
 
--- frontend/ayd-storage/src/components/layout/MainLayout.jsx
+++ frontend/ayd-storage/src/components/layout/MainLayout.jsx
@@ -1,34 +1,130 @@
-import { Box, Toolbar } from '@mui/material';
+import React from 'react';
+import { Box, Toolbar, AppBar, Drawer } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
 import sizeConfigs from '../../configs/sizeConfigs';
 import colorConfigs from '../../configs/colorConfigs';
 // import Topbar from '../common/Topbar';
 import Sidebar from '../common/SideBar/Sidebar';
 import { Outlet } from 'react-router-dom';
+import MenuIcon from '@mui/icons-material/Menu';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
 
 const MainLayout = () => {
+
+    const [mobileOpen, setMobileOpen] = React.useState(false);
+    const [isClosing, setIsClosing] = React.useState(false);
+
+    const handleDrawerClose = () => {
+        setIsClosing(true);
+        setMobileOpen(false);
+    };
+
+    const handleDrawerTransitionEnd = () => {
+        setIsClosing(false);
+    };
+
+    const handleDrawerToggle = () => {
+        if (!isClosing) {
+            setMobileOpen(!mobileOpen);
+        }
+    };
+
     return (
-        <Box sx={{ display: "flex"}}>
+        <Box sx={{ display: "flex" }}>
+            <CssBaseline />
+            <AppBar
+                position="fixed"
+                sx={{
+                    width: { sm: `calc(100% - ${sizeConfigs.sidebar.width}px)` },
+                    ml: { sm: `${sizeConfigs.sidebar.width}px` },
+                    backgroundColor: colorConfigs.sidebar.bg,
+                    display: { sm: 'none' }
+                }}
+            >
+                <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        edge="start"
+                        onClick={handleDrawerToggle}
+                        sx={{ mr: 2, display: { sm: 'none' } }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" noWrap component="div">
+                    </Typography>
+                </Toolbar>
+            </AppBar>
             {/* <Topbar /> */}
             <Box
                 component="nav"
                 sx={{
-                    width: sizeConfigs.sidebar.width,
-                    flexShrink: 0
+                    width: {sm: sizeConfigs.sidebar.width},
+                    flexShrink: { sm: 0 }
+                    
                 }}
+                aria-label="XD"
             >
-                <Sidebar />
+
+                <Drawer
+                    variant="temporary"
+                    open={mobileOpen}
+                    onTransitionEnd={handleDrawerTransitionEnd}
+                    onClose={handleDrawerClose}
+                    ModalProps={{
+                        keepMounted: true, // Better open performance on mobile.
+                    }}
+                    sx={{
+                        display: { xs: "block", sm: "none" },
+                        width: sizeConfigs.sidebar.width,
+                        flexShrink: 0,
+                        "& .MuiDrawer-paper": {
+                            width: sizeConfigs.sidebar.width,
+                            boxSizing: "border-box",
+                            borderRight: "0px",
+                            backgroundColor: colorConfigs.sidebar.bg,
+                            color: colorConfigs.sidebar.color,
+                        },
+                    }}
+                >
+                    <Sidebar/>
+                </Drawer>
+
+
+                <Drawer
+                    variant="permanent"
+                    sx={{
+                        display: { xs: "none", sm: "block" },
+                        width: sizeConfigs.sidebar.width,
+                        flexShrink: 0,
+                        "& .MuiDrawer-paper": {
+                            width: sizeConfigs.sidebar.width,
+                            boxSizing: "border-box",
+                            borderRight: "0px",
+                            backgroundColor: colorConfigs.sidebar.bg,
+                            color: colorConfigs.sidebar.color,
+                        },
+                    }}
+                    open
+                >
+                    <Sidebar/>
+                </Drawer>
             </Box>
             <Box
                 component="main"
                 sx={{
                     flexGrow: 1,
                     p: 0,
-                    width: `calc(100% - ${sizeConfigs.sidebar.width}px)`,
-                    minHeight: "100vh",
+                    width: { sm: `calc(100% - ${sizeConfigs.sidebar.width}px)` },
                     backgroundColor: colorConfigs.mainBg
                 }}
             >
-                <Toolbar />
+                <Toolbar
+                    sx={{
+                        display: { sm: 'none' }
+                    }}
+                />
                 <Outlet />
             </Box>
         </Box>
