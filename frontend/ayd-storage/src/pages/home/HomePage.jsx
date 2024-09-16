6265fc5ffff875b79ce6121d97af50ea958da289
Steven González <stevengonzalez088@gmail.com>
2024-09-16 17:21:19 -0600

    [UPDATE ] se actualiza redirección desde login - 201903984

--- frontend/ayd-storage/src/pages/home/HomePage.jsx
+++ frontend/ayd-storage/src/pages/home/HomePage.jsx
@@ -1,10 +1,27 @@
-import React from "react";
-import SettingsMenu from "../../components/Admin/SettingsMenu";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+    const navigate = useNavigate();
+    useEffect(() => {
+        const tipoUsuario = localStorage.getItem('USUARIO') ? JSON.parse(localStorage.getItem('USUARIO')).ROL : null
+        if (tipoUsuario) {
+            if (tipoUsuario === 1) { // si es admin mandarlo a la page de admin
+                //navigate('/admin')
+            } else if (tipoUsuario === 2) { // si es cliente mandarlo a la pagina de archivos
+                navigate('/files')
+            } else if (tipoUsuario === 3) { // si es cliente mandarlo a la pagina de archivos
+                //navigate('/empleado')
+            }
+        } else {
+            localStorage.removeItem('USUARIO')
+            navigate('/')
+        }
+
+    }, [])
+
     return (
         <div>
-            <SettingsMenu />
         </div>
     )
 }
