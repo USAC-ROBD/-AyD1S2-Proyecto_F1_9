22be5754467a7b2d844f564213b50c71fe415703
Douglas <dougdari@gmail.com>
2024-09-30 16:10:53 -0600

    [UPDATE] modificación de la consulta para setear el eliminado de la cuenta

--- backend/controllers/processDeleteRequest.mjs
+++ backend/controllers/processDeleteRequest.mjs
@@ -9,7 +9,7 @@ const processDeleteRequest = async (req, res) => {
 
         if (state === "2") {
             const account = await db.query("SELECT ID_CUENTA FROM SOLICITUD_ELIMINAR_CUENTA WHERE ID_SOLICITUD = ?", [id]);
-            await db.query("UPDATE CUENTA SET ELIMINADO = ? WHERE ID_CUENTA = ?", ["1", account[0].ID_CUENTA]);
+            await db.query("UPDATE CUENTA SET ELIMINADO = ? WHERE ID_CUENTA = ?", ["1", account[0][0].ID_CUENTA]);
         }
         return res.status(200).json({ "status": 200, "message": "Solicitud de cambio de almacenamiento actualizada " + configurations.host + ":" + configurations.port });
     } catch (error) {
