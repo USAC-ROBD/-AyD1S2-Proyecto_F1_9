9fee741825c3800e0fd6cc1cea4c6ecf0b0c8177
Steven González <stevengonzalez088@gmail.com>
2024-10-23 19:11:57 -0600

    [UPDATE] Tests

--- backend/__tests__/profile.test.js
+++ backend/__tests__/profile.test.js
@@ -2,7 +2,7 @@ import request from 'supertest'
 import app from '../server.mjs'
 
 test('POST /uploadProfile, debe retornar un atributo status=200, icon, message', async () => {
-    const res = await request(app).post('/uploadProfile').send({ ID_USUARIO: 2, changes: {NOMBRE: 'John', APELLIDO: 'Connor'}})
+    const res = await request(app).post('/uploadProfile').send({ ID_USUARIO: 2, changes: {NOMBRE: 'Steven', APELLIDO: 'Gonzalez'}})
     expect(res.status).toBe(200);
     expect(res.body).toHaveProperty('status', 200);
     expect(res.body).toHaveProperty('icon');
