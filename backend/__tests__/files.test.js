95151b5c6c1cd1ce20feffede8896afebef17ceb
robinbuezo11 <robinbuezo11@gmail.com>
2024-10-25 23:22:04 -0600

    [ADD] Se agregaron las pruebas unitarias de los favoritos"

--- backend/__tests__/files.test.js
+++ backend/__tests__/files.test.js
@@ -30,3 +30,27 @@ test('POST /createFolder, debe retornar un atributo status=200, y el id del fold
     expect(res.body).toHaveProperty('status', 200);
     expect(res.body).toHaveProperty('folder');
 }, 30000); // 30 segundos de timeout
+
+
+test('POST /getFavsItems, debe retornar un status=200 y una lista de children', async () => {
+  const res = await request(app).post('/getFavorites').send({
+      idAccount: 1,
+      idFolder: null // Cambia según lo que quieras probar
+  });
+  expect(res.status).toBe(200);
+  expect(res.body).toHaveProperty('status', 200);
+  expect(res.body).toHaveProperty('children');
+  expect(Array.isArray(res.body.children)).toBe(true);
+}, 30000);
+
+
+test('POST /setFavItem, debe retornar un status=200 y el nuevo estado de favorito', async () => {
+  const res = await request(app).post('/setFavorite').send({
+      idItem: 1, // Cambia este ID a un valor válido para la prueba
+      type: 'file' // Cambia a 'folder' si necesitas probar una carpeta
+  });
+  expect(res.status).toBe(200);
+  expect(res.body).toHaveProperty('status', 200);
+  expect(res.body).toHaveProperty('fav');
+  expect(typeof res.body.fav).toBe('number'); // El nuevo estado de favorito debería ser 0 o 1
+}, 30000);
